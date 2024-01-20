06e049794705f37c10f2093271b155d0efa1a11d
nadaeezzdin <nadaeezzdin@gmail.com>
2024-01-20 05:31:09 +0200

    add options route

--- notesBackend/src/index.php
+++ notesBackend/src/index.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use NotesApp\Controllers\NoteController;
@@ -7,6 +6,13 @@ use NotesApp\Controllers\UserController;
 use NotesApp\Models\NoteModel;
 use NotesApp\Models\UserModel;
 use NotesApp\Utils\Database;
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+header('Access-Control-Allow-Credentials: true');
+
 
 
 
@@ -22,17 +28,16 @@ $userController = new UserController($database, $userModel);
 
 //routes
 
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
-
 $request_uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 $base_path = '/luftborn-task/notesBackend/src/index.php';
 
-
-if ($method === 'POST' && $request_uri === $base_path .'/register') {
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization');
+    header('Access-Control-Max-Age: 86400'); 
+}
+elseif ($method === 'POST' && $request_uri === $base_path .'/register') {
     $userController->register();
 } elseif ($method === 'POST' && $request_uri === $base_path .'/login'){
     $userController->logIn();
@@ -48,7 +53,7 @@ elseif ($method === 'GET' && $request_uri === $base_path .'/notes') {
 } elseif ($method === 'PUT' && preg_match('/\/notes\?id=\d+/', $request_uri)) {
     $noteController->updateNote();
 } elseif ($method === 'DELETE' && preg_match('/\/notes\?id=\d+/', $request_uri)) {
-    $noteController->DeleteNote();
+    $noteController->deleteNote();
 } else {
     http_response_code(404);
     echo json_encode(['message' => 'Not Found']);
