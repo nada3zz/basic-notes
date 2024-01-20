150e300d4bb8b0fec0a1847b3774238a927cda13
nadaeezzdin <nadaeezzdin@gmail.com>
2024-01-20 05:27:35 +0200

    add router

--- notes-frontend/src/App.tsx
+++ notes-frontend/src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import LogInPage from "./pages/LogIn";
+import RegisterPage from './pages/Register';
 
 
 function App() {
   return (
-
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LogInPage />} />
+          <Route path="/register" element={< RegisterPage/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
