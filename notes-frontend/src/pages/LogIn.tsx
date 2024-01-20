09f3e68050ccd7dd94f20ea4c976af16aa8b48ef
nadaeezzdin <nadaeezzdin@gmail.com>
2024-01-20 06:45:49 +0200

    add navbar

--- notes-frontend/src/pages/LogIn.tsx
+++ notes-frontend/src/pages/LogIn.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Login from "../components/AuthForm/LogIn";
+import NavBar from "../components/NavBar/NavBar";
 import './../components/AuthForm/AuthForm.scss';
 
 const LogInPage: React.FC = () => {
   return (
+    <div>
+      <NavBar></NavBar>
     <div className="form-container">
       <Login />
+    </div>
     </div>
   );
 };
--- notes-frontend/src/pages/Register.tsx
+++ notes-frontend/src/pages/Register.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Signup from '../components/AuthForm/Register';
+import NavBar from "../components/NavBar/NavBar";
 import './../components/AuthForm/AuthForm.scss';
 
 const RegisterPage: React.FC = () => {
   return (
+    <div>
+    <NavBar></NavBar>
     <div className="form-container">
       <Signup />
+    </div>
     </div>
   );
 };
