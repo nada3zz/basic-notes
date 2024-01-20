116ac2ea03fc0bbb57d18b3e87448d6bea619eeb
nadaeezzdin <nadaeezzdin@gmail.com>
2024-01-20 05:21:53 +0200

    add auth forms & validation

--- notes-frontend/src/components/AuthForm/LogIn.tsx
+++ notes-frontend/src/components/AuthForm/LogIn.tsx
@@ -1,46 +1,57 @@
 import React, { useState, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import AuthService from './../../services/authService';
+import './AuthForm.scss';
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState<string | null>(null);
 
   const navigate = useNavigate();
 
   const handleLogin = async (e: FormEvent) => {
     e.preventDefault();
     try {
-      await AuthService.login(email, password).then(
-        () => {
-          navigate("/home");
-          window.location.reload();
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
+     const response = await AuthService.login(email, password);
+     if (response.status === "success") {
+      //console.log("login", response)
+      navigate("/");
+      window.location.reload();
+     } else {
+      setError(response.message || "Login Failed")
+     }
+
     } catch (err) {
       console.log(err);
+      setError("An error occurred. Please try again.");
     }
   };
 
   return (
-    <div>
+    <div className="form">
       <form onSubmit={handleLogin}>
         <h3>Login</h3>
+        {error && <p style={{ color: 'red' }}>{error}</p>} 
+        <div>
+        <label htmlFor="email">Email</label>
         <input
           type="text"
           placeholder="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
+        </div>
+
+        <div>
+        <label htmlFor="password">Password</label>
         <input
           type="password"
           placeholder="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+        </div>
         <button type="submit">Log in</button>
       </form>
     </div>
--- notes-frontend/src/components/AuthForm/Register.tsx
+++ notes-frontend/src/components/AuthForm/Register.tsx
@@ -1,56 +1,77 @@
 import React, { useState, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import AuthService from "../../services/authService";
-
+import './AuthForm.scss';
 
 const Signup: React.FC = () => {
   const [username, setusername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState<string | null>(null);
 
   const navigate = useNavigate();
 
   const handleSignup = async (e: FormEvent) => {
     e.preventDefault();
+
+    if (!username || !email || !password) {
+      setError("All fields are required");
+      return;
+    }
+
     try {
-      await AuthService.signup(email, password, username).then(
-        (response) => {
-          //console.log("Sign up successfully", response);
-          navigate("/home");
-          window.location.reload();
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
+      const response = await AuthService.signup(username, email, password);
+      if (response.status === 'success') {
+        //console.log("Sign up", response);
+        navigate("/");
+        window.location.reload();
+      } else {
+        console.log("Sign up", response);
+        setError(response.message || "Signup failed");
+      }
+
     } catch (err) {
       console.log(err);
+      setError("An error occurred. Please try again.");
     }
   };
 
   return (
-    <div>
+    <div className="form">
       <form onSubmit={handleSignup}>
-        <h3>Sign up</h3>
+        <h2>Register</h2>
+         {error && <p style={{ color: 'red' }}>{error}</p>} 
+        <div>
+        <label htmlFor="username">Username</label>
         <input
           type="text"
           placeholder="username"
-          value={email}
+          value={username}
           onChange={(e) => setusername(e.target.value)}
         />
+        </div>
+        <div>
+        <label htmlFor="Email">Email</label>
         <input
           type="text"
           placeholder="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
+        </div>
+
+        <div>
+        <label htmlFor="password">password</label>
         <input
           type="password"
           placeholder="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+        </div>
+        <div>
         <button type="submit">Sign up</button>
+        </div>
       </form>
     </div>
   );
